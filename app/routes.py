643fb0de51ed453c9626fc029a947fee2669b406
GabrielBarbosaSouza <gabrielbarbosasouzabiel@gmail.com>
2026-08-13 08:05:30 -0300

    Criando variáveis e jogando elas para o front-end

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for
 
 
 @app.route('/')
 def homepage():
-    return render_template('index.html')
+    usuario = "Gabriel Souza"
+    idade = 18
+    
+    context = {
+        'usuario': usuario,
+        'idade': idade
+    }
+    
+    return render_template('index.html', context=context)
+
+@app.route('/teste/')
+def novapag():
+    return "Você está na segunda página!"
