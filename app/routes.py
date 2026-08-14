da4faabd471feedd03ce1be397547ffef77c9b58
Gabriel Souza <ti002@brkmegadan.local>
2026-08-14 09:53:38 -0300

    Criei um HTML simples do zero com bootstrap. Fiz um formulário via HTML para testar as requisições GET e POST. Confuso no inicio, mas consegui :D

--- app/routes.py
+++ app/routes.py
@@ -4,25 +4,32 @@ from flask import render_template, url_for, request
 
 @app.route('/')
 def homepage():
+    # variaveis
     usuario = "Gabriel Souza"
     idade = 18
     
+    #context para salvar as variaveis e poder lançar elas pro HTML
     context = {
         'usuario': usuario,
         'idade': idade
     }
     
-    return render_template('index.html', context=context)
+    return render_template('index.html', context=context) # retorna a página index e o context
+
 
 @app.route('/contato/', methods=['GET', 'POST'])
-def novapag():
+def contato():
     context = {}
     
     if request.method == 'GET':
-        pesquisa = request.args.get('pesquisa')
-        context.update({
-            'pesquisa': pesquisa
-        })
+        pesquisa = request.args.get('pesquisa') # pega os argumentos da URL
+        print(f"GET: {pesquisa}") # printa a respota no terminal
         
-    return render_template('contato.html', context=context)
-    
+        context.update({'pesquisa': pesquisa}) # atualiza para a pesquisa não aparecer na URL
+        
+    if request.method == 'POST':
+            pesquisa = request.form['pesquisa'] # pega a resposta do formulário POST
+            print(f"POST: {pesquisa}") # printa a resposta no terminal
+
+            
+    return render_template('contato.html', context=context) # retorna a página de contato e o context
